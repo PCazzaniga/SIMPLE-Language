f8e72e39e16c5a95921c0fad53c9644d492e3241
PCazzaniga <72246753+PCazzaniga@users.noreply.github.com>
2026-02-15 17:27:59 +0100

    Updated version number & rebuilt jar package

--- src/simpleInterpreter.java
+++ src/simpleInterpreter.java
@@ -253,7 +253,7 @@ public class simpleInterpreter {
 		private static void printHelp(){
 			String helpMsg =
 					"""
-					SIMPLE v1.5.0 Copyright (C) 2025 - 2026 PCazzaniga (github.com)
+					SIMPLE v1.6.0 Copyright (C) 2025 - 2026 PCazzaniga (github.com)
 					This program is distributed under the GNU General Public License Version 3
 					
 					Interpreter for the S.I.M.P.L.E. programming language, validates and optionally executes a .simple file.
